322011a75a6cc0e39fd7683b1218dddda1f7dcaf
Diego Borigen <diego@iddigital.com.au>
2024-04-02 08:38:04 +1100

    Remove get static paths

--- app/projects/[projectsId]/page.tsx
+++ app/projects/[projectsId]/page.tsx
@@ -1,24 +1,12 @@
 import getProject from "@/API/getProject";
-import getAllProjects from "@/API/getAllProjects";
 import ProjectDetail from "@/components/ProjectDetail";
 import { Project } from "@/src/types";
-import { GetStaticPaths } from "next";
 
 type Params = {
     params: {
         projectsId: number
     }
 }
-
-export const getStaticPaths: GetStaticPaths = async () => {
-    const projects = await getAllProjects();
-
-    const paths = projects.map((project:Project) => ({
-      params: { projectsId: project.id.toString() },
-    }));
-
-    return { paths, fallback: true };
-};
 
 export default async function ProjectPage({ params: {projectsId}}: Params) {
     const project: null | Project = await getProject(projectsId);
