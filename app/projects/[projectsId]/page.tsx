a7ef84caf9d896ef768c96fd837c04544180470d
Diego Borigen <diego@iddigital.com.au>
2024-04-01 21:02:44 +1100

    Add GetStaticPaths to project

--- app/projects/[projectsId]/page.tsx
+++ app/projects/[projectsId]/page.tsx
@@ -1,12 +1,24 @@
 import getProject from "@/API/getProject";
+import getAllProjects from "@/API/getAllProjects";
 import ProjectDetail from "@/components/ProjectDetail";
 import { Project } from "@/src/types";
+import { GetStaticPaths } from "next";
 
 type Params = {
     params: {
         projectsId: number
     }
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const projects = await getAllProjects();
+
+    const paths = projects.map((project:Project) => ({
+      params: { projectsId: project.id.toString() },
+    }));
+
+    return { paths, fallback: true };
+};
 
 export default async function ProjectPage({ params: {projectsId}}: Params) {
     const project: null | Project = await getProject(projectsId);
